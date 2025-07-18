8e9f7e25114c92a27366e113d2fa82589b07961b
Branislav Kontur <bkontur@gmail.com>
2025-07-18 16:47:33 +0200

    Compilation nits

--- runtime/src/panic_handler.rs
+++ runtime/src/panic_handler.rs
@@ -3,12 +3,12 @@
 // TODO: Remove this file once `sp-io` is upgraded.
 //       See https://github.com/paritytech/polkadot-bulletin-chain/issues/18.
 
-use sp_core::LogLevel;
+use sp_core::LogLevelFilter;
 
 #[cfg(not(feature = "std"))]
 #[panic_handler]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
 	let message = alloc::format!("{}", info);
-	sp_io::logging::log(LogLevel::Error, "runtime", message.as_bytes());
+	sp_io::logging::log(LogLevelFilter::Error, "runtime", message.as_bytes());
 	unreachable!();
 }
