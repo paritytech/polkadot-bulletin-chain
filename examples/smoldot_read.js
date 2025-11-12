0a2f7bede0bbc5ca959d6e6610069da6c634dad6
ndk <ndk@parity.io>
2025-11-12 15:30:52 +0100

    added read example with smalldot

---
+++ examples/smoldot_read.js
@@ -0,0 +1,17 @@
+import * as smoldot from 'smoldot';
+import fs from 'fs';
+
+const chainSpec = fs.readFileSync('./node/chain-specs/bulletin-polkadot.json', 'utf8');
+const client = smoldot.start( {logCallback: LogCallback});
+
+await client
+    .addChain({ chainSpec })
+    .then((chain) => {
+        chain.sendJsonRpc(`{"jsonrpc":"2.0","id":12,"method":"chain_getFinalizedHead","params":[]}`);
+        return chain;
+      })
+      .then(async (chain) => {
+        const response = await chain.nextJsonRpcResponse();
+        console.log("✅ JSON-RPC response:", response);
+      })
+      .then(() => client.terminate())
