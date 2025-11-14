4eee06157646d0789a4ec7ab49c120ff0d264aaf
ndk <ndk@parity.io>
2025-11-14 09:43:32 +0100

    added smoldot + read to live chain

--- examples/smoldot_read.js
+++ examples/smoldot_read.js
@@ -1,17 +1,32 @@
 import * as smoldot from 'smoldot';
-import fs from 'fs';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
-const chainSpec = fs.readFileSync('./node/chain-specs/bulletin-polkadot.json', 'utf8');
-const client = smoldot.start();
 
-await client
-    .addChain({ chainSpec })
-    .then((chain) => {
-        chain.sendJsonRpc(`{"jsonrpc":"2.0","id":12,"method":"chain_getFinalizedHead","params":[]}`);
-        return chain;
-      })
-      .then(async (chain) => {
-        const response = await chain.nextJsonRpcResponse();
-        console.log("✅ JSON-RPC response:", response);
-      })
-      .then(() => client.terminate())
+async function main() {
+    await cryptoWaitReady();
+
+    const ws = new WsProvider('wss://bulletin.rpc.amforc.com');
+    const api = await ApiPromise.create({ provider: ws });
+    await api.isReady;
+
+    // true: raw chain spec
+    const chainSpec = await api.rpc.syncstate.genSyncSpec(true);
+
+    const client = smoldot.start();
+
+    await client
+            .addChain({ chainSpec: chainSpec.toString() })
+            .then((chain) => {
+                chain.sendJsonRpc(`{"jsonrpc":"2.0","id":12,"method":"chain_getFinalizedHead","params":[]}`);
+                return chain;
+            })
+            .then(async (chain) => {
+                const response = await chain.nextJsonRpcResponse();
+                console.log("✅ JSON-RPC response:", response);
+                return chain;
+            })
+            .then(() => client.terminate());
+}
+
+await main();
