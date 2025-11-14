3fc5af26e2ddabce4cfc686758d6832592da0e8e
Branislav Kontur <bkontur@gmail.com>
2025-11-14 23:59:05 +0100

    Update examples/smoldot_read.js

--- examples/smoldot_read.js
+++ examples/smoldot_read.js
@@ -6,11 +6,13 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 async function main() {
     await cryptoWaitReady();
 
-    const ws = new WsProvider('wss://bulletin.rpc.amforc.com');
+    // Download the raw chain spec from an RPC node.
+    // Note: The dApps or the actual application should not connect to any external RPC node,
+    //       but should instead use chain specs provided directly.    
+     // 12346 is Bob's validator rpc port, because Alice does not have `bootNodes`.
+    const ws = new WsProvider('ws://localhost:12346');
     const api = await ApiPromise.create({ provider: ws });
     await api.isReady;
-
-    // true: raw chain spec
     const chainSpec = await api.rpc.syncstate.genSyncSpec(true);
 
     const client = smoldot.start();
