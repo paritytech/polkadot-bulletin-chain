c029f4ef3d2ec5890966125fcc7caae3154311a3
Branislav Kontur <bkontur@gmail.com>
2025-11-19 12:17:59 +0100

    Tmp fix for IPFS reconnect for para

---
+++ scripts/ipfs-reconnect-solo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Peers to monitor
+PEERS_TO_CHECK=(
+    "/ip4/172.17.0.1/tcp/10001/ws/p2p/12D3KooWQCkBm1BYtkHpocxCwMgR8yjitEeHGx8spzcDLGt2gkBm"
+    "/ip4/172.17.0.1/tcp/12347/ws/p2p/12D3KooWRkZhiRhsqmrQ28rt73K7V3aCBpqKrLGSXmZ99PTcTZby"
+)
+
+while true; do
+    # Read all current connections once
+    PEERS=$(docker exec ipfs-node ipfs swarm peers)
+
+    for PEER in "${PEERS_TO_CHECK[@]}"; do
+        echo "$PEERS" | grep -q "$PEER"
+        if [ $? -ne 0 ]; then
+            echo "$(date) - $PEER disconnected. Reconnecting..."
+            docker exec ipfs-node ipfs swarm connect "$PEER"
+        else
+            echo "$(date) - $PEER connected."
+        fi
+    done
+
+    sleep 2
+done
