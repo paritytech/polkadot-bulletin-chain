9246d9c361aa5dfd542b6f6b82608c32b1a93f84
Branislav Kontur <bkontur@gmail.com>
2025-11-20 14:46:58 +0100

    Apply suggestions from code review

--- scripts/ipfs-reconnect-solo.sh
+++ scripts/ipfs-reconnect-solo.sh
@@ -2,19 +2,19 @@
 
 # Peers to monitor
 PEERS_TO_CHECK=(
-    "/ip4/127.0.0.1/tcp/10001/ws/p2p/12D3KooWQCkBm1BYtkHpocxCwMgR8yjitEeHGx8spzcDLGt2gkBm"
-    "/ip4/127.0.0.1/tcp/12347/ws/p2p/12D3KooWRkZhiRhsqmrQ28rt73K7V3aCBpqKrLGSXmZ99PTcTZby"
+    "/ip4/172.17.0.1/tcp/10001/ws/p2p/12D3KooWQCkBm1BYtkHpocxCwMgR8yjitEeHGx8spzcDLGt2gkBm"
+    "/ip4/172.17.0.1/tcp/12347/ws/p2p/12D3KooWRkZhiRhsqmrQ28rt73K7V3aCBpqKrLGSXmZ99PTcTZby"
 )
 
 while true; do
     # Read all current connections once
-    PEERS=$(./kubo/ipfs swarm peers)
+    PEERS=$(docker exec ipfs-node swarm peers)
 
     for PEER in "${PEERS_TO_CHECK[@]}"; do
         echo "$PEERS" | grep -q "$PEER"
         if [ $? -ne 0 ]; then
             echo "$(date) - $PEER disconnected. Reconnecting..."
-            ./kubo/ipfs swarm connect "$PEER"
+            docker exec ipfs-node ipfs swarm connect "$PEER"
         else
             echo "$(date) - $PEER connected."
         fi
