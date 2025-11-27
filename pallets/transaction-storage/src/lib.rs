f33c8a9903f7fb6dc47d108ad856bd708cb9f092
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-11-27 14:04:41 +0000

    Add `hash` field to `Event::Stored` and `Event::Renewed` (#124)

--- pallets/transaction-storage/src/lib.rs
+++ pallets/transaction-storage/src/lib.rs
@@ -329,7 +329,7 @@ pub mod pallet {
 					})
 					.map_err(|_| Error::<T>::TooManyTransactions)
 			})?;
-			Self::deposit_event(Event::Stored { index });
+			Self::deposit_event(Event::Stored { index, hash: content_hash });
 			Ok(())
 		}
 
@@ -361,7 +361,8 @@ pub mod pallet {
 
 			let extrinsic_index =
 				<frame_system::Pallet<T>>::extrinsic_index().ok_or(Error::<T>::BadContext)?;
-			sp_io::transaction_index::renew(extrinsic_index, info.content_hash.into());
+			let content_hash = info.content_hash.into();
+			sp_io::transaction_index::renew(extrinsic_index, content_hash);
 
 			let mut index = 0;
 			<BlockTransactions<T>>::mutate(|transactions| {
@@ -377,7 +378,7 @@ pub mod pallet {
 					})
 					.map_err(|_| Error::<T>::TooManyTransactions)
 			})?;
-			Self::deposit_event(Event::Renewed { index });
+			Self::deposit_event(Event::Renewed { index, hash: content_hash });
 			Ok(().into())
 		}
 
@@ -558,9 +559,9 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Stored data under specified index.
-		Stored { index: u32 },
+		Stored { index: u32, hash: ContentHash },
 		/// Renewed data under specified index.
-		Renewed { index: u32 },
+		Renewed { index: u32, hash: ContentHash },
 		/// Storage proof was successfully checked.
 		ProofChecked,
 		/// An account `who` was authorized to store `bytes` bytes in `transactions` transactions.
@@ -667,10 +668,10 @@ pub mod pallet {
 					// enough lifetime for their store/renew transactions that they aren't at risk
 					// of replay when the account is next authorized.
 					if let Err(err) = frame_system::Pallet::<T>::dec_providers(who) {
-						log::warn!(
+						tracing::warn!(
 							target: LOG_TARGET,
-							"Failed to decrement provider reference count for authorized account {who:?}, \
-							leaking reference: {err:?}"
+							error=?err, ?who,
+							"Failed to decrement provider reference count for authorized account leaking reference"
 						);
 					}
 				},
