3d21e5752027c1e898d224f4563befd52866609f
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-12-05 09:26:36 +0000

    Remove NoopCurrency

--- pallets/transaction-storage/src/lib.rs
+++ pallets/transaction-storage/src/lib.rs
@@ -34,18 +34,9 @@ mod tests;
 
 use codec::{Decode, Encode, MaxEncodedLen};
 use polkadot_sdk_frame::{
-	deps::{
-		sp_core::sp_std::{marker::PhantomData, prelude::*},
-		*,
-	},
-	prelude::{
-		fungible::{hold::DoneSlash, Dust},
-		*,
-	},
-	traits::fungible::{
-		Balanced, BalancedHold, Inspect, InspectHold, Mutate, MutateHold, Unbalanced,
-		UnbalancedHold,
-	},
+	deps::{sp_core::sp_std::prelude::*, *},
+	prelude::*,
+	traits::fungible::{Balanced, Mutate, MutateHold},
 };
 use sp_transaction_storage_proof::{
 	encode_index, num_chunks, random_chunk, ChunkIndex, InherentError, TransactionStorageProof,
@@ -84,128 +75,6 @@ pub struct AuthorizationExtent {
 	pub transactions: u32,
 	/// Number of bytes.
 	pub bytes: u64,
-}
-
-/// A no-op fungible implementation for this pallet.
-///
-/// All balances are always zero and every mutation is accepted but ignored. This is useful for
-/// runtimes that do not want to charge any currency like Bulletin.
-pub struct NoopCurrency<Reason>(PhantomData<Reason>);
-
-impl<AccountId, Reason> Balanced<AccountId> for NoopCurrency<Reason>
-where
-	Reason: 'static + Encode + TypeInfo,
-{
-	type OnDropDebt = ();
-	type OnDropCredit = ();
-}
-
-impl<AccountId, Reason> BalancedHold<AccountId> for NoopCurrency<Reason> where
-	Reason: Encode + TypeInfo + 'static
-{
-}
-
-impl<AccountId, Reason, Balance> DoneSlash<Reason, AccountId, Balance> for NoopCurrency<Reason>
-where
-	Balance: Sized,
-	Reason: 'static + Encode + TypeInfo,
-{
-}
-
-impl<AccountId, Reason> Inspect<AccountId> for NoopCurrency<Reason> {
-	type Balance = u64;
-
-	fn total_issuance() -> Self::Balance {
-		0
-	}
-
-	fn minimum_balance() -> Self::Balance {
-		0
-	}
-
-	fn total_balance(_who: &AccountId) -> Self::Balance {
-		0
-	}
-
-	fn balance(_who: &AccountId) -> Self::Balance {
-		0
-	}
-
-	fn reducible_balance(
-		_who: &AccountId,
-		_preservation: Preservation,
-		_force: Fortitude,
-	) -> Self::Balance {
-		0
-	}
-
-	fn can_deposit(
-		_who: &AccountId,
-		_amount: Self::Balance,
-		_provenance: Provenance,
-	) -> DepositConsequence {
-		DepositConsequence::Blocked
-	}
-
-	fn can_withdraw(
-		_who: &AccountId,
-		_amount: Self::Balance,
-	) -> WithdrawConsequence<Self::Balance> {
-		WithdrawConsequence::Frozen
-	}
-}
-
-impl<AccountId, Reason> InspectHold<AccountId> for NoopCurrency<Reason>
-where
-	Reason: Encode + TypeInfo + 'static,
-{
-	type Reason = Reason;
-
-	fn total_balance_on_hold(_who: &AccountId) -> Self::Balance {
-		0
-	}
-
-	fn balance_on_hold(_reason: &Self::Reason, _who: &AccountId) -> Self::Balance {
-		0
-	}
-}
-
-impl<AccountId, Reason> Unbalanced<AccountId> for NoopCurrency<Reason> {
-	fn handle_dust(_dust: Dust<AccountId, Self>) {}
-
-	fn write_balance(
-		_who: &AccountId,
-		_amount: Self::Balance,
-	) -> Result<Option<Self::Balance>, DispatchError> {
-		Ok(Some(Zero::zero()))
-	}
-
-	fn set_total_issuance(_amount: Self::Balance) {}
-}
-
-impl<AccountId, Reason> UnbalancedHold<AccountId> for NoopCurrency<Reason>
-where
-	Reason: Encode + TypeInfo + 'static,
-{
-	fn set_balance_on_hold(
-		_reason: &Self::Reason,
-		_who: &AccountId,
-		_amount: Self::Balance,
-	) -> sp_runtime::DispatchResult {
-		Ok(())
-	}
-}
-
-impl<AccountId, Reason> Mutate<AccountId> for NoopCurrency<Reason>
-where
-	AccountId: Eq,
-	Reason: Encode + TypeInfo + 'static,
-{
-}
-
-impl<AccountId, Reason> MutateHold<AccountId> for NoopCurrency<Reason> where
-	Reason: Encode + TypeInfo + 'static
-{
 }
 
 /// Hash of a stored blob of data.
