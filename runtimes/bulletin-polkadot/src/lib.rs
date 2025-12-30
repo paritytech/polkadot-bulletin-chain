b6da30cd03ca11ae0d703552f7211000f52b1d3b
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-12-30 00:17:57 +0000

    Align `StoragePeriod` between Bulletin and SDK (#134)
    * Align `StoragePeriod` between Bulletin and SDK
    
    * Sync mock
    
    * Remove StoragePeriod
    
    * Use default
    
    * Update integrity_test
    
    * Revert "Update integrity_test"
    
    This reverts commit 0a4946161c228cd00e6ae33f84f2b3ea4f8ead4c.
    
    * Revert "Use default"
    
    This reverts commit 91536d4f8e89af316b6928209c82751f1212399e.
    
    * Update integrity_test
    
    * Fix clippy
    
    * Sync with SDK
    
    * Update TODO
    
    * Update errors
    
    * Fix fmt
    
    * Reflect pallet changes from https://github.com/paritytech/polkadot-sdk/pull/10662
    
    * Remove unused constants
    
    * Setup single vs mbm migrations
    
    * Add migration `SetRetentionPeriodIfZero` to ensure RetentionPeriod is non-zero for live solochain
    
    * Add post-upgrade check
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>

--- runtimes/bulletin-polkadot/src/lib.rs
+++ runtimes/bulletin-polkadot/src/lib.rs
@@ -193,8 +193,6 @@ parameter_types! {
 		EquivocationReportPeriodInEpochs::get() * (EPOCH_DURATION_IN_BLOCKS as u64);
 
 
-	// This currently _must_ be set to DEFAULT_STORAGE_PERIOD
-	pub const StoragePeriod: BlockNumber = sp_transaction_storage_proof::DEFAULT_STORAGE_PERIOD;
 	pub const AuthorizationPeriod: BlockNumber = 7 * DAYS;
 	pub const StoreRenewPriority: TransactionPriority = RemoveExpiredAuthorizationPriority::get() - 1;
 	pub const StoreRenewLongevity: TransactionLongevity = DAYS as TransactionLongevity;
@@ -254,6 +252,9 @@ impl frame_system::Config for Runtime {
 	/// Weight information for the extrinsics of this pallet.
 	type SystemWeightInfo = weights::frame_system::WeightInfo<Runtime>;
 	type ExtensionsWeightInfo = weights::frame_system_extensions::WeightInfo<Runtime>;
+
+	type SingleBlockMigrations = migrations::SingleBlockMigrations;
+	type MultiBlockMigrator = migrations::MbmMigrations;
 }
 
 impl pallet_validator_set::Config for Runtime {
@@ -351,7 +352,6 @@ impl pallet_transaction_storage::Config for Runtime {
 	type MaxBlockTransactions = ConstU32<512>;
 	/// Max transaction size per block needs to be aligned with [`BlockLength`].
 	type MaxTransactionSize = ConstU32<{ 8 * 1024 * 1024 }>;
-	type StoragePeriod = StoragePeriod;
 	type AuthorizationPeriod = AuthorizationPeriod;
 	type Authorizer = EnsureRoot<Self::AccountId>;
 	type StoreRenewPriority = StoreRenewPriority;
@@ -716,6 +716,27 @@ pub type Executive = frame_executive::Executive<
 	Runtime,
 	AllPalletsWithSystem,
 >;
+
+/// The runtime migrations per release.
+#[allow(deprecated, missing_docs)]
+pub mod migrations {
+	/// Unreleased migrations. Add new ones here:
+	pub type Unreleased = ();
+
+	/// Migrations/checks that do not need to be versioned and can run on every update.
+	pub type Permanent = (
+		pallet_transaction_storage::migrations::SetRetentionPeriodIfZero<
+			crate::Runtime,
+			pallet_transaction_storage::DefaultRetentionPeriod,
+		>,
+	);
+
+	/// All single block migrations that will run on the next runtime upgrade.
+	pub type SingleBlockMigrations = (Unreleased, Permanent);
+
+	/// MBM migrations to apply on runtime upgrade.
+	pub type MbmMigrations = ();
+}
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benches {
