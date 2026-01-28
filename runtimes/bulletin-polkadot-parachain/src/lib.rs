22ae5d3a2dda61e59d1374b76dbc72740c9abfb0
Anthony Kveder <anthony.kveder@parity.io>
2026-01-28 22:55:13 +0000

    Added ProvideCidConfig extension

--- runtimes/bulletin-polkadot-parachain/src/lib.rs
+++ runtimes/bulletin-polkadot-parachain/src/lib.rs
@@ -124,6 +124,7 @@ pub type TxExtension = cumulus_pallet_weight_reclaim::StorageWeightReclaim<
 		>,
 		ValidateSigned,
 		frame_metadata_hash_extension::CheckMetadataHash<Runtime>,
+		pallet_transaction_storage::extension::ProvideCidConfig<Runtime>,
 	),
 >;
 
