1c9a596caf05168e6a4a22e68c48dc78835f0d82
Naren Mudigal <naren@parity.io>
2026-02-20 14:36:17 +0100

    chore: rebuild documentation

--- console-ui/public/docs/searcher-c2a407aa.js
+++ console-ui/public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a8a936f7.js',
+                path_to_root + 'searchindex-c08dc049.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
