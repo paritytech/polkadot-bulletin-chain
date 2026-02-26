932265af9a62bf0a018f06eedb0bbfce8fef99a2
Karol Kokoszka <karol@parity.io>
2026-02-26 17:22:38 +0100

    Add comment about PoV percentage

--- runtimes/bulletin-westend/tests/tests.rs
+++ runtimes/bulletin-westend/tests/tests.rs
@@ -667,6 +667,8 @@ fn transaction_storage_weight_sanity() {
 	pallet_transaction_storage::ensure_weight_sanity::<Runtime>(
 		&block_weights,
 		normal_length,
+		// Collator-side PoV cap: default 85% of max_pov_size.
+		// See cumulus/client/consensus/aura/src/collators/slot_based/block_builder_task.rs
 		Some(85),
 	);
 }
