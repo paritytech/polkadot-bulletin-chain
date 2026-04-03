d3e39f9616e4e1c5d3b6364b63496e757021dde0
Karol Kokoszka <karol@parity.io>
2026-04-03 18:27:33 +0200

    Add new account as authorizer + change authorize temporarily to feeless (#381)

--- runtimes/bulletin-westend/src/storage.rs
+++ runtimes/bulletin-westend/src/storage.rs
@@ -37,7 +37,17 @@ use sp_runtime::transaction_validity::{TransactionLongevity, TransactionPriority
 pub struct TestAccounts;
 impl SortedMembers<AccountId> for TestAccounts {
 	fn sorted_members() -> Vec<AccountId> {
-		alloc::vec![Sr25519Keyring::Alice.to_account_id()]
+		let mut members = alloc::vec![
+			Sr25519Keyring::Alice.to_account_id(),
+			// 5GBhBA9H49M24LaZXaQopm3MzHtBT9i4mbQZbMSn5FcJNRb9
+			AccountId::new([
+				0xb6, 0x45, 0x5b, 0xc5, 0x38, 0x36, 0x5d, 0x32, 0xd3, 0x29, 0x67, 0xb6, 0xf2, 0x1a,
+				0x0c, 0x9b, 0x07, 0x15, 0x65, 0xe8, 0x78, 0xfe, 0x98, 0x5f, 0x88, 0xd1, 0x54, 0x3c,
+				0xb1, 0x99, 0x1a, 0x7d,
+			]),
+		];
+		members.sort();
+		members
 	}
 }
 
