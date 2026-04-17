09973959ecc7e565d4d43772e0d5576efd695c1a
ndk <ndk@parity.io>
2026-04-17 10:58:28 +0200

    Bump bulletin-westend spec_version to 1_000_011

--- runtimes/bulletin-westend/src/lib.rs
+++ runtimes/bulletin-westend/src/lib.rs
@@ -185,7 +185,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bulletin-westend"),
 	impl_name: alloc::borrow::Cow::Borrowed("bulletin-westend"),
 	authoring_version: 1,
-	spec_version: 1_000_010,
+	spec_version: 1_000_011,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
