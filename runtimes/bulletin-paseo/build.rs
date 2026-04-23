0b2d0f71bfa888d03eb583bda408b97df63792b8
Branislav Kontur <bkontur@gmail.com>
2026-04-23 16:48:32 +0200

    Update runtimes/bulletin-paseo/build.rs

--- runtimes/bulletin-paseo/build.rs
+++ runtimes/bulletin-paseo/build.rs
@@ -26,13 +26,13 @@ fn main() {
 #[cfg(all(feature = "metadata-hash", feature = "std"))]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
-		.enable_metadata_hash("WND", 12)
+		.enable_metadata_hash("PAS", 10)
 		.build();
 
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
 		.set_file_name("fast_runtime_binary.rs")
 		.enable_feature("fast-runtime")
-		.enable_metadata_hash("WND", 12)
+		.enable_metadata_hash("PAS", 10)
 		.build();
 }
 
