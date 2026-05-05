3b91cad8a033ba8249ef2ecfd3b1d199c62847ab
Karol Kokoszka <karol@parity.io>
2026-05-05 20:11:55 +0200

    Bump westend & paseo spec_version to 1_000_013 (#496)
    * Bump westend spec_version to 1_000_013
    
    * Bump paseo spec_version to 1_000_013

--- runtimes/bulletin-westend/src/lib.rs
+++ runtimes/bulletin-westend/src/lib.rs
@@ -190,7 +190,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bulletin-westend"),
 	impl_name: alloc::borrow::Cow::Borrowed("bulletin-westend"),
 	authoring_version: 1,
-	spec_version: 1_000_012,
+	spec_version: 1_000_013,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
