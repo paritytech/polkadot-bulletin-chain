98bd60ff00f0e8456e7a3482ec6d1e9791289a68
Rohit Sarpotdar <rohit.sarpotdar@parity.io>
2026-05-05 15:16:52 +0530

    fresh weights for paseo & westend runtimes (#483)
    * Bump to unstable2604
    
    * Add hop-promotion to the crates release
    
    * Replace sp-hop
    
    * Add `is_promoted_on_chain` impl
    
    * Fmt
    
    * Added indexed_transactions runtime api
    
    * Update Cargo.toml
    
    Co-authored-by: Karol Kokoszka <karol@parity.io>
    
    * fresh weights for bulletin-paseo runtime
    
    * fresh weights for bulletin-westend runtime
    
    * Update from danielbui12 running command 'bench --runtime bulletin-paseo'
    
    * Update from github-actions[bot] running command 'bench --runtime bulletin-westend'
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: Karol Kokoszka <karol@parity.io>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pallets/transaction-storage/src/lib.rs
+++ pallets/transaction-storage/src/lib.rs
@@ -767,7 +767,7 @@ pub mod pallet {
 	/// Collection of transaction metadata by block number.
 	#[pallet::storage]
 	#[pallet::getter(fn transaction_roots)]
-	pub(super) type Transactions<T: Config> = StorageMap<
+	pub type Transactions<T: Config> = StorageMap<
 		_,
 		Blake2_128Concat,
 		BlockNumberFor<T>,
