573a9f0f32c918ac320cba3cf274930cb183fccc
Branislav Kontur <bkontur@gmail.com>
2026-06-17 15:55:13 +0200

    Bump paseo runtime spec_version to 1_000_019

--- runtimes/bulletin-paseo/src/lib.rs
+++ runtimes/bulletin-paseo/src/lib.rs
@@ -179,7 +179,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bulletin-paseo"),
 	impl_name: alloc::borrow::Cow::Borrowed("bulletin-paseo"),
 	authoring_version: 1,
-	spec_version: 1_000_018,
+	spec_version: 1_000_019,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
