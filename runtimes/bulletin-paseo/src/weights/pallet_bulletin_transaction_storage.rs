0346b52ce55b9b104a51f28d3688230dcce3fa02
Karol Kokoszka <karol@parity.io>
2026-08-05 07:38:32 +0200

    Placeholder

--- runtimes/bulletin-paseo/src/weights/pallet_bulletin_transaction_storage.rs
+++ runtimes/bulletin-paseo/src/weights/pallet_bulletin_transaction_storage.rs
@@ -1,4 +1,4 @@
-// REGENERATE ME
+// REGENERATE MEE
 // Copyright (C) Parity Technologies and the various Polkadot contributors, see CONTRIBUTING.md
 // for a list of specific contributors.
 // SPDX-License-Identifier: Apache-2.0
