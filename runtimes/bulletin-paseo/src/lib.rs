0305c765441a404480f612a53a0d99cd3d71f161
Branislav Kontur <bkontur@gmail.com>
2026-08-11 17:35:40 +0200

    Bump westend and paseo spec_version to 1_000_023 (#744)
    * Bump westend and paseo spec_version to 1_000_023
    
    * Update from github-actions[bot] running command 'bench --runtime bulletin-paseo'
    
    * Update from github-actions[bot] running command 'bench --runtime bulletin-westend'
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- runtimes/bulletin-paseo/src/lib.rs
+++ runtimes/bulletin-paseo/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bulletin-paseo"),
 	impl_name: alloc::borrow::Cow::Borrowed("bulletin-paseo"),
 	authoring_version: 1,
-	spec_version: 1_000_022,
+	spec_version: 1_000_023,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
