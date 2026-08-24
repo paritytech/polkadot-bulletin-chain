f7ba6cbc3f2fa625d332191e9de56077ec46d86e
Rohit Sarpotdar <rohit.sarpotdar@parity.io>
2026-08-24 23:50:40 +0530

    all migrations are done for paseo & westend
    Co-authored-by: Karol Kokoszka <karol.k91@gmail.com>
    Co-authored-by: Rohit Sarpotdar <rohi7ns@gmail.com>

--- runtimes/bulletin-westend/src/lib.rs
+++ runtimes/bulletin-westend/src/lib.rs
@@ -143,7 +143,7 @@ pub mod migrations {
 		cumulus_pallet_xcmp_queue::migration::v6::MigrateV5ToV6<Runtime>,
 		cumulus_pallet_xcmp_queue::migration::v7::MigrateV6ToV7<Runtime>,
 		cumulus_pallet_parachain_system::migration::Migration<Runtime>,
-	);
+	pub type Unreleased = ();
 
 	/// Migrations/checks that do not need to be versioned and can run on every update.
 	pub type Permanent = (
