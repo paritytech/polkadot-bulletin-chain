b74571d82255f24cbb6079cb07c68a33cabb52e2
Rohit Sarpotdar <rohit.sarpotdar@parity.io>
2026-08-24 23:53:56 +0530

    all migrations are complete, removing unreleased migrations

--- runtimes/bulletin-westend/src/lib.rs
+++ runtimes/bulletin-westend/src/lib.rs
@@ -131,18 +131,6 @@ pub mod migrations {
 	use super::*;
 
 	/// Unreleased migrations. Add new ones here:
-	pub type Unreleased = (
-		pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,
-		cumulus_pallet_xcmp_queue::migration::v4::MigrationToV4<Runtime>,
-		cumulus_pallet_xcmp_queue::migration::v5::MigrateV4ToV5<Runtime>,
-		pallet_session::migrations::v1::MigrateV0ToV1<
-			Runtime,
-			pallet_session::migrations::v1::InitOffenceSeverity<Runtime>,
-		>,
-		cumulus_pallet_aura_ext::migration::MigrateV0ToV1<Runtime>,
-		cumulus_pallet_xcmp_queue::migration::v6::MigrateV5ToV6<Runtime>,
-		cumulus_pallet_xcmp_queue::migration::v7::MigrateV6ToV7<Runtime>,
-	pub type Unreleased = (
 	pub type Unreleased = ();
 
 	/// Migrations/checks that do not need to be versioned and can run on every update.
